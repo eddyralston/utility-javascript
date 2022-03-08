4117e3fb9c4f9c707564eb60440e277f10f55c75
eddyralston <eddyralston@outlook.com>
2022-03-08 13:57:36 -0600

    Update utility.js

--- utility.js
+++ utility.js
@@ -5,3 +5,28 @@ const html = string =>{
  }
 
 const id = id =>document.getElementById(id)
+
+
+/* --- async function order ---  */
+var action = {
+    list:{},
+    define(name,callback){
+        this.list[name]=callback
+    },
+    run(name,doneCB){
+        this.list[name](doneCB)
+    }
+}
+function runner(...method){
+    var i = 0
+    var loop = () => {
+        var name = method[i]
+        action.run(name,()=>{
+            i++;
+            console.log('done:'+name);
+            if(i<method.length)loop();
+        })
+    }
+    loop()
+}
+/*  --------   */
