e17047ec287f67798045be70a4946a0432bba626
eddyralston <eddyralston@outlook.com>
2022-04-22 03:08:30 -0600

    Update approuter.js

--- approuter.js
+++ approuter.js
@@ -1,30 +1,58 @@
-function AppRouter () {
+class Router{
+    constructor(){
     this.routes = {}
-    this.on = function (path,callback){
+    }
+    on(path,callback){
         this.routes[path] = callback
     }
-    this.check = function (){
-
+    check(){
         var hash = '/'+window.location.hash.substring(1)
-        console.log(hash)
-        hash.replaceAll(Regex,function(){
-            var path = arguments[1]
-            var query = arguments[2]
-            console.log(path,query)
-        })
-        this.routes[hash]()
+        var hash = hash.replaceAll('$','][$').replaceAll('?','][?').replaceAll('&','][&').split('][')
+        var activeRoute = null
+        var paramsObj = {}
+        var flagsArr = []
+        hash.forEach(params => {
+            var type = params[0]
+            if(type=='/')activeRoute = this.routes[hash[0]]
+            if(type=='$')flagsArr.push(params.substr(1))
+            if(type=='?'){
+                var KeyValue = params.substr(1).split('=')
+                var key = KeyValue[0]
+                var value = KeyValue[1]
+                paramsObj[key]=value
+            }
+
+        });
+        activeRoute({params:paramsObj,flags:flagsArr})
     }
-    this.href = function(path){
-        window.location.hash=path
+    open(path,{params,flags}){
+        var url = path
+        for (i in params) url+='?'+i+'='+params
+        flags.forEach(flag=>url+='$'+flag)
+        window.location.hash=url
     }
 }
 
-var router = new AppRouter()
-router.on('/',function(self){
-    document.body.innerHTML+='index page'
-})
-router.on('/main',function(self){
-    document.getElementById('app').innerHTML+='main page'+self
-    })
 window.addEventListener('load', ()=>router.check());
 window.addEventListener('hashchange', ()=>router.check());
+const element = (tag,prop)=>{
+    var el = document.createElement(tag)
+    if(prop){
+        for (props in prop){
+            el[props] = prop[props]
+        }
+    }
+    return el
+}
+function html(string){
+    var el = element('div',{innerHTML:string})
+    return el.firstElementChild;
+}
+function ShadowElement(callback){
+var host = element('div')
+host.attachShadow({mode:'open'})
+var shadowRoot = host.shadowRoot
+callback(shadowRoot)
+return host
+}
+
