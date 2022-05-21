2de6314adf984816c24e978b5178ee7f2e717fb6
eddyralston <eddyralston@outlook.com>
2022-05-21 03:46:08 -0600

    Delete ww.js

--- ww.js
+++
@@ -1,7 +0,0 @@
-onmessage=e=>{
-    var t1 = performance.now()
-    var result = e.data.num1 ** e.data.num2 ** e.data.num3
-    var t2 = performance.now()
-    var delay = t2 - t1
-    postMessage({result,delay});
-}
